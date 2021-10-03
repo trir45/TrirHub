22e518a53179fbafc4194f157f6ffc90d5ea4981
trir45 <84477433+trir45@users.noreply.github.com>
2021-10-03 14:32:46 -0800

    Add files via upload

---
+++ TrirHub.lua
@@ -0,0 +1,19 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Trir Hub for Pet Simulator X", "BloodTheme")
+local Tab = Window:NewTab("GUI'S")
+local Section = Tab:NewSection("GUI's")
+Section:NewButton("PSX Grey GUI", "poggers", function()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Alleexxi/releasedscritps/main/script%20(1).lua'),true))()    
+print("loaded PSX Grey GUI B) ")
+end)
+
+Section:NewButton("PFN GUI", "poggers", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BY-PFN/Pet-Simulator-X-/main/Pet%20Simulator%20X%20ByPFN%20.lua", true))()
+print("loaded PFN GUI B) ")
+end)
+
+
+Section:NewButton("EzPets", "poggers", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TurfuGoldy/GoldenScripts/main/EzPets.lua"))()
+print("loaded EzPets B) ")
+end)
